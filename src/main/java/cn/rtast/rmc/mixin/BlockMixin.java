51fcb9435c6a7352fd2e9677a98ae0cb865dc526
RTAkland <rtakland@outlook.com>
2024-04-14 12:25:48 +0800

    update

--- src/main/java/cn/rtast/rmc/mixin/BlockMixin.java
+++ src/main/java/cn/rtast/rmc/mixin/BlockMixin.java
@@ -25,12 +25,13 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Block.class)
 public class BlockMixin {
+
     @Inject(method = "onBreak", at = @At("HEAD"))
-    public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player, CallbackInfo ci) {
+    public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player, CallbackInfoReturnable<BlockState> cir) {
         player.increaseStat(RMC.RMC_STAT_ID, 1);
     }
 }
